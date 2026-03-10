abc68ecabcd835b37ab7f50e446d0d7f5cb58a2e
Aadilgyan <aadil04.com@gmail.com>
2026-03-10 09:53:21 +0530

    AADIL Added UC1

---
+++ APP/src/PalindromeCheckerApp.java
@@ -0,0 +1,17 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("=====================================");
+        System.out.println("      Welcome to Palindrome App      ");
+        System.out.println("=====================================");
+
+        // Display application details
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Version          : 1.0");
+
+        System.out.println("-------------------------------------");
+        System.out.println("Application Started Successfully!");
+        System.out.println("-------------------------------------");
+
+}
