f3ec1a0e3593ff33713d55ec285b627f2731b214
Aadilgyan <aadil04.com@gmail.com>
2026-03-10 10:21:00 +0530

    AADIL Added UC4

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,22 +1,34 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        // Original string
+        // Input string
         String input = "madam";
 
-        // Variable to store reversed string
-        String reversed = "";
+        // Convert string to character array
+        char[] chars = input.toCharArray();
 
-        // Reverse the string using loop
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+        int start = 0;
+        int end = chars.length - 1;
+
+        boolean isPalindrome = true;
+
+        // Two-pointer comparison
+        while (start < end) {
+
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
         }
 
         // Display input
         System.out.println("Input text: " + input);
 
-        // Compare original and reversed
-        if (input.equals(reversed)) {
+        // Display result
+        if (isPalindrome) {
             System.out.println("Is it a Palindrome? : true");
         } else {
             System.out.println("Is it a Palindrome? : false");
